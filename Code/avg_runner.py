941634cfedc4e2f479d5d744b38424cec2140b70
Sirebellum <joshua.herrera227@gmail.com>
2019-05-26 09:33:03 -0500

    Proper save directory for models

--- Code/avg_runner.py
+++ Code/avg_runner.py
@@ -101,7 +101,7 @@ class AVGRunner:
                 print( '-' * 30)
                 print( 'Saving models...')
                 self.saver.save(self.sess,
-                                c.MODEL_SAVE_DIR + 'model.ckpt',
+                                c.MODEL_SAVE_DIR + '/model.ckpt',
                                 global_step=self.global_step)
                 print( 'Saved models!')
                 print( '-' * 30)
